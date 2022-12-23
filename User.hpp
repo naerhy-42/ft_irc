f91e07d9207f44d12c47f82481a2fd614d184887
ddordain <ddordain@student.42.fr>
2022-12-23 22:23:10 +0100

    other classes needed in the project

---
+++ User.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Socket.hpp"
+
+class User
+{
+public:
+    User();
+    ~User();
+
+    // Nickname
+    std::string const &get_nick() const;
+    void    set_nick(std::string const &nick);
+    bool    is_valid_nick(std::string const &nick);
+    // Channels
+    std::vector<std::string> const &channels() const;
+    void    join_channel(std::string const &channel);
+    void    part_channel(std::string const &channel);
+
+    // Error message
+    void    send_to_client(Socket &socket, std::string const &prefix, std::string const &command, std::vector<std::string> const &params);
+    void    send_error_message(Socket &socket, std::string const &prefix, std::string const &error_code, std::string const &error_message);
+
+private:
+    std::string                 _nick;
+    std::vector<std::string>    _channels;
+};
