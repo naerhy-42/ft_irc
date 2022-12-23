83ff287c0b34a97f656cb3c553bdee7dfb88670d
ddordain <ddordain@student.42.fr>
2022-12-23 23:47:37 +0100

    indentation

--- Client.hpp
+++ Client.hpp
@@ -6,25 +6,25 @@
 
 class Client
 {
-public:
-
-    Client();
-    ~Client();
-
-    // Connect to the IRC server
-    void connect(std::string const &server, std::string const &port);
-
-    // Disconnect from the IRC server
-    void disconnect();
-
-private:
-    // Socket for the connection to the IRC server
-    Socket _socket;
-
-    // Protocol handler for the IRC connection : Access to all the message handlers
-    Protocol _protocol;
+    public:
+    
+        Client();
+        ~Client();
+    
+        // Connect to the IRC server
+        void connect(std::string const &server, std::string const &port);
+    
+        // Disconnect from the IRC server
+        void disconnect();
+    
+    private:
+        // Socket for the connection to the IRC server
+        Socket _socket;
+    
+        // Protocol handler for the IRC connection : Access to all the message handlers
+        Protocol _protocol;
 };
 
 
-// the final goal, code like this:
+// The final goal, code like this:
 // client.get_protocol().send_message("NICK mynick");
