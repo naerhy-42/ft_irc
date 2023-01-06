2c13fe71de21debc0e70923683e5a8a413f4388c
ddordain <ddordain@student.42.fr>
2023-01-06 11:30:12 +0100

    upload

--- Reply.cpp
+++ Reply.cpp
@@ -7,7 +7,7 @@ namespace ft
                             const std::string &nick, const std::string &user,
                             const std::string &host)
     {
-        return client + " :Welcome to the " + networkname + " Network, " + nick +
+        return ":irc.forty-two.com 001 " + client + " :Welcome to the " + networkname + " Network, " + nick +
                "[!" + user + "@" + host + "]";
     }
 
@@ -15,13 +15,13 @@ namespace ft
     std::string rpl_yourhost(const std::string &client, const std::string &hostname,
                              const std::string &version)
     {
-        return client + " :Your host is " + hostname + ", running version " + version;
+        return ":irc.forty-two.com 002 " + client + " :Your host is " + hostname + ", running version " + version;
     }
 
     // RPL_CREATED (003)
     std::string rpl_created(const std::string &client, const std::string &date)
     {
-        return client + " :This server was created " + date;
+        return ":irc.forty-two.com 003 " + client + " :This server was created " + date;
     }
 
     // RPL_MYINFO (004)
@@ -30,33 +30,33 @@ namespace ft
                            const std::string &available_channel_modes,
                            const std::string &channel_modes_with_parameter)
     {
-        return client + " " + servername + " " + version + " " + available_user_modes +
+        return ":irc.forty-two.com 004 " + client + " " + servername + " " + version + " " + available_user_modes +
                " " + available_channel_modes + " [" + channel_modes_with_parameter + "]";
     }
 
     // RPL_ISSUPPORT (005)
     std::string rpl_isupport(const std::string &client, const std::string &tokens)
     {
-        return client + " " + tokens + " :are supported by this server";
+        return ":irc.forty-two.com 005 " + client + " " + tokens + " :are supported by this server";
     }
 
     // RPL_BOUNCE (010)
     std::string rpl_bounce(const std::string &client, const std::string &hostname,
                            int port, const std::string &info)
     {
-        return client + " " + hostname + " " + std::to_string(port) + " :" + info;
+        return ":irc.forty-two.com 010 " + client + " " + hostname + " " + std::to_string(port) + " :" + info;
     }
 
     // RPL_UMODEIS (221)
     std::string rpl_umodeis(const std::string &client, const std::string &user_modes)
     {
-        return client + " " + user_modes;
+        return ":irc.forty-two.com 221 " + client + " " + user_modes;
     }
 
     // RPL_LUSERSCHANNELS (254)
     std::string rpl_luserchannels(const std::string &client, int channels)
     {
-        return client + " " + std::to_string(channels) + " :channels formed";
+        return ":irc.forty-two.com 254 " + client + " " + std::to_string(channels) + " :channels formed";
     }
 
     // RPL_WHOREPLY (352)
@@ -66,7 +66,7 @@ namespace ft
                              const std::string &flags, int hopcount,
                              const std::string &realname)
     {
-        return client + " " + channel + " " + username + " " + host + " " +
+        return ":irc.forty-two.com 352 " + client + " " + channel + " " + username + " " + host + " " +
                server + " " + nick + " " + flags + " " + std::to_string(hopcount) +
                " " + realname;
     }
@@ -74,13 +74,13 @@ namespace ft
     // RPL_ENDOFWHO (315)
     std::string rpl_endofwho(const std::string &client, const std::string &mask)
     {
-        return client + " " + mask + " :End of WHO list";
+        return ":irc.forty-two.com 315 " + client + " " + mask + " :End of WHO list";
     }
 
     // RPL_WHOISREGNICK (307)
     std::string rpl_whoisregnick(const std::string &client, const std::string &nick)
     {
-        return client + " " + nick + " :has identified for this nick";
+        return ":irc.forty-two.com 307 " + client + " " + nick + " :has identified for this nick";
     }
 
     // RPL_WHOISUSER (311)
@@ -88,7 +88,7 @@ namespace ft
                               const std::string &username, const std::string &host,
                               const std::string &realname)
     {
-        return client + " " + nick + " " + username + " " + host + " * :" + realname;
+        return ":irc.forty-two.com 311 " + client + " " + nick + " " + username + " " + host + " * :" + realname;
     }
 
     // RPL_WHOWASUSER (314)
@@ -96,20 +96,20 @@ namespace ft
                                const std::string &username, const std::string &host,
                                const std::string &realname)
     {
-        return client + " " + nick + " " + username + " " + host + " * :" + realname;
+        return ":irc.forty-two.com 314 " + client + " " + nick + " " + username + " " + host + " * :" + realname;
     }
 
     // RPL_ENDOFWHOIS (318)
     std::string rpl_endofwhois(const std::string &client, const std::string &nick)
     {
-        return client + " " + nick + " :End of /WHOIS list";
+        return ":irc.forty-two.com 318 " + client + " " + nick + " :End of /WHOIS list";
     }
 
     // RPL_WHOISCHANNELS (319)
     std::string rpl_whoischannels(const std::string &client, const std::string &nick,
                                   const std::vector<std::pair<char, std::string>> &channels)
     {
-        std::string message = client + " " + nick + " :";
+        std::string message = ":irc.forty-two.com 319 " + client + " " + nick + " :";
         for (unsigned int i = 0; i < channels.size(); ++i)
         {
             message += channels[i].first + channels[i].second + " ";
@@ -120,38 +120,38 @@ namespace ft
     // RPL_LISTSTART (321)
     std::string rpl_liststart(const std::string &client)
     {
-        return client + " Channel :Users Name";
+        return ":irc.forty-two.com 321 " + client + " Channel :Users Name";
     }
 
     // RPL_LIST (322)
     std::string rpl_list(const std::string &client, const std::string &channel, int client_count,
                          const std::string &topic)
     {
-        return client + " " + channel + " " + std::to_string(client_count) + " :" + topic;
+        return ":irc.forty-two.com 322 " + client + " " + channel + " " + std::to_string(client_count) + " :" + topic;
     }
 
     // RPL_LISTEND (323)
     std::string rpl_listend(const std::string &client)
     {
-        return client + " :End of /LIST";
+        return ":irc.forty-two.com 323 " + client + " :End of /LIST";
     }
 
     // RPL_INVITELIST (336)
     std::string rpl_invitelist(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel;
+        return ":irc.forty-two.com 336 " + client + " " + channel;
     }
 
     // RPL_ENDOFINVITELIST (337)
     std::string rpl_endofinvitelist(const std::string &client)
     {
-        return client + " :End of /INVITE list";
+        return ":irc.forty-two.com 337 " + client + " :End of /INVITE list";
     }
 
     // RPL_INVITING (341)
     std::string rpl_inviting(const std::string &client, const std::string &nick, const std::string &channel)
     {
-        return client + " " + nick + " " + channel;
+        return ":irc.forty-two.com 341 " + client + " " + nick + " " + channel;
     }
 
     // RPL_BANLIST (367)
@@ -159,7 +159,7 @@ namespace ft
                             const std::string &mask, const std::string &who,
                             long set_ts)
     {
-        std::string message = client + " " + channel + " " + mask;
+        std::string message = ":irc.forty-two.com 367 " + client + " " + channel + " " + mask;
         if (!who.empty())
             message += " " + who;
         if (set_ts > 0)
@@ -170,7 +170,7 @@ namespace ft
     // RPL_ENDOFBANLIST (368)
     std::string rpl_endofbanlist(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :End of channel ban list";
+        return ":irc.forty-two.com 368 " + client + " " + channel + " :End of channel ban list";
     }
 
     // RPL_ENDOFWHOWAS (369)
@@ -184,7 +184,7 @@ namespace ft
                          const std::string &timestamp, int ts_offset,
                          const std::string &human_readable_time)
     {
-        std::string message = client + " " + server + " ";
+        std::string message = ":irc.forty-two.com 391 " + client + " " + server + " ";
         if (!timestamp.empty())
         {
             message += timestamp + " ";
@@ -201,7 +201,7 @@ namespace ft
     std::string err_unknownerror(const std::string &client, const std::string &command,
                                  const std::string &subcommand, const std::string &info)
     {
-        std::string message = client + " " + command;
+        std::string message = ":irc.forty-two.com 400 " + client + " " + command;
         if (!subcommand.empty())
         {
             message += " " + subcommand;
@@ -213,147 +213,147 @@ namespace ft
     // ERR_NOSUCHNICK (401)
     std::string err_nosuchnick(const std::string &client, const std::string &nickname)
     {
-        return client + " " + nickname + " :No such nick/channel";
+        return ":irc.forty-two.com 401 " + client + " " + nickname + " :No such nick/channel";
     }
 
     // ERR_NOSUCHSERVER (402)
     std::string err_nosuchserver(const std::string &client, const std::string &server_name)
     {
-        return client + " " + server_name + " :No such server";
+        return ":irc.forty-two.com 402 " + client + " " + server_name + " :No such server";
     }
 
     // ERR_NOSUCHCHANNEL (403)
     std::string err_nosuchchannel(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :No such channel";
+        return ":irc.forty-two.com 403 " + client + " " + channel + " :No such channel";
     }
 
     // ERR_CANNOTSENDTOCHAN (404)
     std::string err_cannotsendtochan(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + ":Cannot send to channel";
+        return ":irc.forty-two.com 404 " + client + " " + channel + ":Cannot send to channel";
     }
 
     // ERR_TOOMANYCHANNELS (405)
     std::string err_toomanychannels(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :You have joined too many channels";
+        return ":irc.forty-two.com 405 " + client + " " + channel + " :You have joined too many channels";
     }
 
     // ERR_WASNOSUCHNICK (406)
     std::string err_wasnosuchnick(const std::string &client, const std::string &nick)
     {
-        return client + " " + nick + " :There was no such nickname";
+        return ":irc.forty-two.com 406 " + client + " " + nick + " :There was no such nickname";
     }
 
     // ERR_NOORIGIN (409)
     std::string err_noorigin(const std::string &client)
     {
-        return client + " :No origin specified";
+        return ":irc.forty-two.com 409 " + client + " :No origin specified";
     }
 
     // ERR_INPUTTOOLONG (417)
     std::string err_inputtoolong(const std::string &client)
     {
-        return client + " :Input line was too long";
+        return ":irc.forty-two.com 417 " + client + " :Input line was too long";
     }
 
     // ERR_UNKNOWNCOMMAND (421)
     std::string err_unknowncommand(const std::string &client, const std::string &command)
     {
-        return client + " " + command + " :Unknown command";
+        return ":irc.forty-two.com 421 " + client + " " + command + " :Unknown command";
     }
 
     // ERR_ERRONEUSNICKNAME (432)
     std::string err_erroneusnickname(const std::string &client, const std::string &nick)
     {
-        return client + " " + nick + " :Erroneous nickname";
+        return ":irc.forty-two.com 432 " + client + " " + nick + " :Erroneous nickname";
     }
 
     // ERR_NICKNAMEINUSE (433)
     std::string err_nicknameinuse(const std::string &client, const std::string &nick)
     {
-        return client + " " + nick + " :Nickname is already in use";
+        return ":irc.forty-two.com 433 " + client + " " + nick + " :Nickname is already in use";
     }
 
     // ERR_USERNOTINCHANNEL (441)
     std::string err_usernotinchannel(const std::string &client, const std::string &nick,
                                      const std::string &channel)
     {
-        return client + " " + nick + " " + channel + " :They aren't on that channel";
+        return ":irc.forty-two.com 441 " + client + " " + nick + " " + channel + " :They aren't on that channel";
     }
 
     // ERR_NOTONCHANNEL (442)
     std::string err_notonchannel(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :You're not on that channel";
+        return ":irc.forty-two.com 442 " + client + " " + channel + " :You're not on that channel";
     }
 
     // ERR_USERONCHANNEL (443)
     std::string err_useronchannel(const std::string &client, const std::string &nick, const std::string &channel)
     {
-        return client + " " + nick + " " + channel + " :is already on channel";
+        return ":irc.forty-two.com 443 " + client + " " + nick + " " + channel + " :is already on channel";
     }
 
     // ERR_NOTREGISTERED (451)
     std::string err_notregistered(const std::string &client)
     {
-        return client + " :You have not registered";
+        return ":irc.forty-two.com 451 " + client + " :You have not registered";
     }
 
     // ERR_NEEDMOREPARAMS (461)
     std::string err_needmoreparams(const std::string &client, const std::string &command)
     {
-        return client + " " + command + " :Not enough parameters";
+        return ":irc.forty-two.com 461 " + client + " " + command + " :Not enough parameters";
     }
 
     // ERR_ALREADYREGISTERED (462)
     std::string err_alreadyregistered(const std::string &client)
     {
-        return client + " :You may not reregister";
+        return ":irc.forty-two.com 462 " + client + " :You may not reregister";
     }
 
     // ERR_PASSWDMISMATCH (464)
     std::string err_passwdmismatch(const std::string &client)
     {
-        return client + " :Password incorrect";
+        return ":irc.forty-two.com 464 " + client + " :Password incorrect";
     }
 
     // ERR_YOUREBANNEDCREEP (465)
     std::string err_yourebannedcreep(const std::string &client)
     {
-        return client + " :You are banned from this server.";
+        return ":irc.forty-two.com 465 " + client + " :You are banned from this server.";
     }
 
     // ERR_CHANNELISFULL (471)
     std::string err_channelisfull(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :Cannot join channel (+l)";
+        return ":irc.forty-two.com 471 " + client + " " + channel + " :Cannot join channel (+l)";
     }
 
     // ERR_UNKNOWNMODE (472)
     std::string err_unknownmode(const std::string &client, char modechar)
     {
-        std::string message = client + " " + modechar + " :is unknown mode char to me";
+        std::string message = ":irc.forty-two.com 472 " + client + " " + modechar + " :is unknown mode char to me";
         return message;
     }
 
     // ERR_INVITEONLYCHAN (473)
     std::string err_inviteonlychan(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :Cannot join channel (+i)";
+        return ":irc.forty-two.com 473 " + client + " " + channel + " :Cannot join channel (+i)";
     }
 
     // ERR_BANNEDFROMCHAN (474)
     std::string err_bannedfromchan(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :Cannot join channel (+b)";
+        return ":irc.forty-two.com 474 " + client + " " + channel + " :Cannot join channel (+b)";
     }
 
     // ERR_BADCHANNELKEY (475)
     std::string err_badchannelkey(const std::string &client, const std::string &channel)
     {
-        return client + " " + channel + " :Cannot join channel (+k)";
+        return ":irc.forty-two.com 475 " + client + " " + channel + " :Cannot join channel (+k)";
     }
 
 }
