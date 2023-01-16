accd5ac474124b76125370ba5a6aeb4ded5075e7
ddordain <ddordain@student.42.fr>
2023-01-16 13:36:48 +0100

    add of the channel class, command join, add channel in privmsg

--- Client.cpp
+++ Client.cpp
@@ -6,31 +6,59 @@ namespace ft
 
 	int Client::get_socket(void) const { return _socket; }
 
-	std::string const& Client::get_nickname(void) const { return _nickname; }
+	std::string const &Client::get_nickname(void) const { return _nickname; }
 
-	std::string const& Client::get_username(void) const { return _username; }
+	std::string const &Client::get_username(void) const { return _username; }
 
-	std::string const& Client::get_hostname(void) const { return _hostname; }
+	std::string const &Client::get_hostname(void) const { return _hostname; }
 
-	std::string const& Client::get_servername(void) const { return _servername; }
+	std::string const &Client::get_servername(void) const { return _servername; }
 
-	std::string const& Client::get_real_name(void) const { return _real_name; }
+	std::string const &Client::get_real_name(void) const { return _real_name; }
 
 	bool Client::get_registration_status(void) const { return _registration_status; }
 
 	bool Client::get_password_status(void) const { return _password_status; }
 
-	void Client::set_nickname(std::string const& nickname) { _nickname = nickname; }
+	void Client::set_nickname(std::string const &nickname) { _nickname = nickname; }
 
-	void Client::set_username(std::string const& username) { _username = username; }
+	void Client::set_username(std::string const &username) { _username = username; }
 
-	void Client::set_hostname(std::string const& hostname) { _hostname = hostname; }
+	void Client::set_hostname(std::string const &hostname) { _hostname = hostname; }
 
-	void Client::set_servername(std::string const& servername) { _servername = servername; }
+	void Client::set_servername(std::string const &servername) { _servername = servername; }
 
-	void Client::set_real_name(std::string const& real_name) { _real_name = real_name; }
+	void Client::set_real_name(std::string const &real_name) { _real_name = real_name; }
 
 	void Client::set_registration_status(bool status) { _registration_status = status; }
 
 	void Client::set_password_status(bool status) { _password_status = status; }
+
+	Client &Client::operator=(Client const &other)
+	{
+		if (this != &other)
+		{
+			_socket = other._socket;
+			_nickname = other._nickname;
+			_username = other._username;
+			_hostname = other._hostname;
+			_servername = other._servername;
+			_real_name = other._real_name;
+			_registration_status = other._registration_status;
+			_password_status = other._password_status;
+		}
+		return *this;
+	}
+
+	bool operator==(Client const &lhs, Client const &rhs)
+	{
+    	return lhs.get_socket() == rhs.get_socket()
+        	&& lhs.get_nickname() == rhs.get_nickname()
+        	&& lhs.get_username() == rhs.get_username()
+        	&& lhs.get_hostname() == rhs.get_hostname()
+        	&& lhs.get_servername() == rhs.get_servername()
+        	&& lhs.get_real_name() == rhs.get_real_name()
+        	&& lhs.get_registration_status() == rhs.get_registration_status()
+        	&& lhs.get_password_status() == rhs.get_password_status();
+	}
 }
--- Client.hpp
+++ Client.hpp
@@ -7,39 +7,44 @@ namespace ft
 {
 	class Client
 	{
-		public:
-			Client(int socket);
-			// Client(Client const& x);
-			// Client& operator=(Client const& x);
-			// ~Client(void);
+	public:
+		Client(int socket);
+		// Client(Client const& x);
+		// Client& operator=(Client const& x);
+		// ~Client(void);
 
-			int get_socket(void) const;
-			std::string const& get_nickname(void) const;
-			std::string const& get_username(void) const;
-			std::string const& get_hostname(void) const;
-			std::string const& get_servername(void) const;
-			std::string const& get_real_name(void) const;
-			bool get_registration_status(void) const;
-			bool get_password_status(void) const;
+		int get_socket(void) const;
+		std::string const &get_nickname(void) const;
+		std::string const &get_username(void) const;
+		std::string const &get_hostname(void) const;
+		std::string const &get_servername(void) const;
+		std::string const &get_real_name(void) const;
+		bool get_registration_status(void) const;
+		bool get_password_status(void) const;
 
-			void set_nickname(std::string const& nickname);
-			void set_username(std::string const& username);
-			void set_hostname(std::string const& hostname);
-			void set_servername(std::string const& servername);
-			void set_real_name(std::string const& real_name);
-			void set_registration_status(bool status);
-			void set_password_status(bool status);
+		void set_nickname(std::string const &nickname);
+		void set_username(std::string const &username);
+		void set_hostname(std::string const &hostname);
+		void set_servername(std::string const &servername);
+		void set_real_name(std::string const &real_name);
+		void set_registration_status(bool status);
+		void set_password_status(bool status);
 
-		private:
-			int _socket;
-			std::string _nickname;
-			std::string _username;
-			std::string _hostname;
-			std::string _servername;
-			std::string _real_name;
-			bool _registration_status;
-			bool _password_status;
+		Client &operator=(Client const &other);
+
+	private:
+		int _socket;
+		std::string _nickname;
+		std::string _username;
+		std::string _hostname;
+		std::string _servername;
+		std::string _real_name;
+		bool _registration_status;
+		bool _password_status;
 	};
+
+	bool operator==(Client const &lhs, Client const &rhs);
+
 }
 
 #endif
