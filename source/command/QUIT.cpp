9d2b17173c62ef4b8f24412e1cf54f8fc314931c
Naerhy <naerhy@protonmail.com>
2023-02-01 10:41:47 +0100

    Remove unnecessary conditions in QUIT

--- source/command/QUIT.cpp
+++ source/command/QUIT.cpp
@@ -6,16 +6,10 @@ namespace ft
     void Protocol::cmd_quit(Message msg)
     {
         Client &client = _get_client_from_socket(msg.get_socket());
+		std::string reply;
 
-		if (!_is_client_connected(client))
-		{
-			std::string reply = err_notregistered(client.get_nickname());
-			add_to_queue(client, reply, 0);
-		}
-        if (client.get_password_status())
-		{
-			_server->close_socket_connection(client.get_socket());
-			// if client is on a channel, send QUIT command to clients in this channel
-		}
+		reply = "ERROR :Closing link\r\n";
+		add_to_queue(client, reply, 0);
+		_server->close_socket_connection(client.get_socket());
     }
 }
