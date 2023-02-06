3efd15d6aed980ac936e28398c2019c54e126a78
Naerhy <naerhy@protonmail.com>
2023-02-06 16:13:24 +0100

    Finalize messages handling in QUIT

--- source/command/QUIT.cpp
+++ source/command/QUIT.cpp
@@ -6,12 +6,7 @@ namespace ft
     void Protocol::cmd_quit(ClientMessage const& cmessage)
     {
         Client* client = cmessage.get_client();
-		std::string client_reply;
-
-		if (cmessage.get_remainder().empty())
-			client_reply = "ERROR :Closing link " + client->get_hostname() + _IRC_ENDL;
-		else
-			client_reply = "ERROR :Closing link " + client->get_hostname() + " :" + cmessage.get_remainder() + _IRC_ENDL;
+		std::string client_reply = "ERROR :Closing link (Client Quit)" + _server.get_hostname() + _IRC_ENDL;
 		send_message_to_client(client, client_reply);
 		ignore_socket(client->get_socket());
 		if (is_client_connected(client))
